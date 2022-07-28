5978037624f0f7283f0e309df4390ec3dca0a5ce
August Duet <august.duet@revature.com>
2022-07-28 12:22:09 -0400

    add myservlet

---
+++ src/main/java/com/ex/web/MyServlet.java
@@ -0,0 +1,8 @@
+package com.ex.web;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = "/web")
+public class MyServlet extends HttpServlet {
+}
