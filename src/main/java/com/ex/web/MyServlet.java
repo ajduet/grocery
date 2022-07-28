3127d882f484eefba2a14809d706f6fa195eb620
August Duet <august.duet@revature.com>
2022-07-28 12:35:50 -0400

    new logic

--- src/main/java/com/ex/web/MyServlet.java
+++ src/main/java/com/ex/web/MyServlet.java
@@ -1,8 +1,18 @@
 package com.ex.web;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(urlPatterns = "/web")
 public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        while(true) {
+
+        }
+    }
 }
